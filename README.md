d0a24535c5a16c993da0a18087c9143e33862627
Gerard Roche <gerardroche@users.noreply.github.com>
2023-05-20 03:55:10 +0100

    Initial version

--- README.md
+++ README.md
@@ -8,4 +8,13 @@
 
 **Pending availability in Package Control.**
 
-Install via [Package Control](https://packagecontrol.io/packages/GitOpen).
+1. Install [`git-open`](https://github.com/paulirish/git-open).
+2. Install plugin via [Package Control](https://packagecontrol.io/packages/GitOpen).
+
+## Commands
+
+Command                         | Description
+:------------------------------ | :----------
+**Git&nbsp;Open&nbsp;**         | Open the page for this branch on the repo website
+**Git&nbsp;Open&nbsp; Commit**  | Open the current commit in the repo website
+**Git&nbsp;Open&nbsp; Issue**   | If this branch is named like issue/\#123, this will open the corresponding issue in the repo website
---
+++ plugin.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+import sys
+
+import sublime_plugin
+
+
+class GitOpenCommand(sublime_plugin.WindowCommand):
+
+    def run(self, commit: bool = False, issue: bool = False) -> None:
+        view = self.window.active_view()
+        if not view:
+            return
+
+        cwd = _get_cwd(view)
+        if not cwd:
+            return
+
+        cmd, shell = _get_args(view, _get_git_open_cmd(commit, issue))
+        subprocess.Popen(cmd, shell=shell, cwd=cwd)
+
+
+def _get_git_open_cmd(commit: bool, issue: bool) -> str:
+    shell_cmd = ['git-open']
+    if commit:
+        shell_cmd.append('--commit')
+    elif issue:
+        shell_cmd.append('--issue')
+
+    return ' '.join(shell_cmd)
+
+
+def _get_args(view, shell_cmd: str) -> tuple:
+    if sys.platform == "win32":
+        shell = True
+        cmd = shell_cmd
+    elif sys.platform == "darwin":
+        cmd = ["/usr/bin/env", "bash", "-l", "-c", shell_cmd]
+        shell = False
+    elif sys.platform == "linux":
+        cmd = ["/usr/bin/env", "bash", "-c", shell_cmd]
+        shell = False
+
+    return (cmd, shell)
+
+
+def _get_cwd(view):
+    file_name = view.file_name()
+    if not file_name:
+        return
+
+    cwd = os.path.dirname(view.file_name())
+    if not os.path.isdir(cwd):
+        return
+
+    return cwd
